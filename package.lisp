ff445c0cc57f4e63e846030cc8fd393569160558
Russ Tyndall <russ@acceleration.net>
2011-05-09 11:27:45 -0400

    use join-strings from adwutils

--- package.lisp
+++ package.lisp
@@ -1,6 +1,7 @@
 (defpackage #:clsql-orm
   (:use "CL" "CLSQL" :iterate :arnesi :adwutils)
   (:shadow #:list-tables )
+  (:shadowing-import-from :adwutils :join-strings)
   (:export :gen-view-class
 	   :gen-view-classes
 	   :gen-view-classes-for-database
