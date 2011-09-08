7aee8247d1c56d8533fb610369a34f49985d5d16
Russ Tyndall <russ@acceleration.net>
2011-09-08 12:36:42 -0400

    whitespace cleanup - github #1

--- clsql-orm.asd
+++ clsql-orm.asd
@@ -2,10 +2,10 @@
 
 ; For those who like that sort of thing: an ASDF package
 (defsystem clsql-orm
-   :depends-on (:clsql :cl-ppcre :cl-interpol :cl-inflector :symbol-munger)
-   :version "0.2"
-   :components
+  :depends-on (:clsql :cl-ppcre :cl-interpol :cl-inflector :symbol-munger)
+  :version "0.2"
+  :components
   :serial T
-   ((:file "package")
-    (:file "main")
-    (:file "sqlite3")))
+  ((:file "package")
+   (:file "main")
+   (:file "sqlite3")))
--- package.lisp
+++ package.lisp
@@ -2,22 +2,22 @@
   (:use :cl :clsql :iterate)
   (:shadow #:list-tables )
   (:export :gen-view-class
-	   :gen-view-classes
-	   :gen-view-classes-for-database
-	   :list-foreign-constraints
-	   :not-null-p
-	   :unique-p
-	   :primary-key-p
-	   :clsql-type-for-pg-type
-	   :list-columns
-           :column-def
-           :column
-           :db-type
-           :spec-type
-           :is-null
-           :scale
-           :col-length
-           :col=
-           :column-diff)
+   :gen-view-classes
+   :gen-view-classes-for-database
+   :list-foreign-constraints
+   :not-null-p
+   :unique-p
+   :primary-key-p
+   :clsql-type-for-pg-type
+   :list-columns
+   :column-def
+   :column
+   :db-type
+   :spec-type
+   :is-null
+   :scale
+   :col-length
+   :col=
+   :column-diff)
   (:documentation "This package provides methods to introspect a database
 Providing features such as generating a CLSQL class based on a table name"))
